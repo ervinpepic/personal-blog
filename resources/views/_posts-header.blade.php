790ecd412f9c0b7e79ae8d6b4a152882ca018a79
Ervin Pepic <pepic__ervin@Ervins-MacBook-Pro.local>
2022-01-17 01:52:59 +0100

    Design glitches fix

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -3,25 +3,18 @@
         Latest <span class="text-blue-500">Laravel From Scratch</span> News
     </h1>
 
-    <h2 class="inline-flex mt-2">By Lary Laracore <img src="/images/lary-head.svg" alt="Head of Lary the mascot"></h2>
-
-    <p class="text-sm mt-14">
-        Another year. Another update. We're refreshing the popular Laravel series with new content.
-        I'm going to keep you guys up to speed with what's going on!
-    </p>
-
-    <div class="space-y-2 lg:space-y-0 lg:space-x-4 mt-8">
+    <div class="space-y-2 lg:space-y-0 lg:space-x-4 mt-4">
         <!--  Category -->
         <div class="relative lg:inline-flex bg-gray-100 rounded-xl">
             <x-dropdown>
                 <x-slot name="trigger">
                     <button
                         class="
-                            py-2 pl-3 pr-9 
-                            text-sm font-semibold 
-                            w-full lg:w-32 
-                            text-left 
-                            flex lg:inline-flex">
+                          py-2 pl-3 pr-9 
+                          text-sm font-semibold 
+                          w-full lg:w-32 
+                          text-left 
+                          flex lg:inline-flex">
                         {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories' }}
 
                         <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
@@ -38,18 +31,17 @@
                 </x-slot>
 
                 <x-dropdown-item href="/" :active="request()->routeIs('home')">All</x-dropdown-item>
-                    @foreach ($categories as $category)
-                        <x-dropdown-item
-                            href="/categories/{{ $category->slug }}"
-                            :active='request()->is("categories/{$category->slug}")'>
-                            {{ ucwords($category->name) }}
-                        </x-dropdown-item>
-                    @endforeach
+                @foreach ($categories as $category)
+                    <x-dropdown-item href="/categories/{{ $category->slug }}"
+                        :active='request()->is("categories/{$category->slug}")'>
+                        {{ ucwords($category->name) }}
+                    </x-dropdown-item>
+                @endforeach
             </x-dropdown>
         </div>
 
         <!-- Other Filters -->
-        <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
+        {{-- <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
             <select class="flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold">
                 <option value="category" disabled selected>Other Filters
                 </option>
@@ -58,8 +50,8 @@
                 <option value="bar">Bar
                 </option>
             </select>
-            <x-icon name="down-arrow" class="absolute pointer-events-none" style="right: 12px"/>
-        </div>
+            <x-icon name="down-arrow" class="absolute pointer-events-none" style="right: 12px" />
+        </div> --}}
 
         <!-- Search -->
         <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
