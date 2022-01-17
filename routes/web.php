a63888ea0672e667b63b98dda5e50071b746566f
Ervin Pepic <pepic__ervin@Ervins-MacBook-Pro.local>
2022-01-17 01:16:55 +0100

    Added factories for databases, aded views and styles, added new componets for dorpdown categories

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,7 +18,8 @@ use App\Models\Post;
 
 Route::get('/', function () {
     return view('posts', [
-        'posts' => Post::with('category')->get()
+        'posts' => Post::latest()->get(),
+        'categories' => Category::all()
     ]);
 });
 
@@ -29,6 +31,15 @@ Route::get('posts/{post:slug}', function (Post $post) {
 
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
-        'posts' => $category->posts
+        'posts' => $category->posts,
+        'currentCategory' => $category,
+        'categories' => Category::all()
+    ]);
+});
+
+Route::get('authors/{author:username}', function (User $author) {
+    return view('posts', [
+        'posts' => $author->posts,
+        'categories' => Category::all()
     ]);
 });
