604bb638b6befac1ea9cad0b791c864bc8732956
Ervin Pepic <pepic__ervin@Ervins-MacBook-Pro.local>
2022-01-29 00:34:33 +0100

    Added comment form

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,14 @@
+<article class="flex bg-gray-100 p-6 rounded-xl border border-gray space-x-4">
+    <div class="flex-shrink-0">
+        <img class="rounded-xl" src="https://i.pravatar.cc/60" alt="Profile Image" width="60" height="60">
+    </div>
+    <div>
+        <header class="mb-2">
+            <h3 class="font-bold">Ervin Pepic</h3>
+            <p>Posted 
+                <time>...time ago..</time>
+            </p>
+        </header>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem totam iure quaerat! Doloremque maiores earum vero explicabo ab iure modi neque quasi, beatae ullam et quo vitae, aliquid reprehenderit exercitationem?</p>
+    </div>
+</article>
