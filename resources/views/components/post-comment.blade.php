46cbac6346d90d457c498f06245496dbaaab06a7
Ervin Pepic <pepic__ervin@Ervins-MacBook-Pro.local>
2022-01-30 01:26:19 +0100

    Added logic for comments

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,14 +1,15 @@
+@props(['comment'])
 <article class="flex bg-gray-100 p-6 rounded-xl border border-gray space-x-4">
     <div class="flex-shrink-0">
-        <img class="rounded-xl" src="https://i.pravatar.cc/60" alt="Profile Image" width="60" height="60">
+        <img class="rounded-xl" src="https://i.pravatar.cc/60?u={{ $comment->id }}" alt="Profile Image" width="60" height="60">
     </div>
     <div>
         <header class="mb-2">
-            <h3 class="font-bold">Ervin Pepic</h3>
+            <h3 class="font-bold">{{ $comment->author->username }}</h3>
             <p>Posted 
-                <time>...time ago..</time>
+                <time>{{ $comment->created_at }}</time>
             </p>
         </header>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem totam iure quaerat! Doloremque maiores earum vero explicabo ab iure modi neque quasi, beatae ullam et quo vitae, aliquid reprehenderit exercitationem?</p>
+        <p>{{ $comment->body }}</p>
     </div>
 </article>
